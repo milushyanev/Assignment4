19aa60c81fb7e81375aace8f6b0defce9013525c
milushyanev <milushyanev@yahoo.com>
2021-10-29 16:42:55 -0700

    Add files via upload

---
+++ Authentication_Assignment/src/model/business/LoginBusiness.java
@@ -0,0 +1,53 @@
+package model.business;
+
+import java.sql.SQLException;
+import model.dataccess.LoginDataAccess;
+import model.entities.MessageException;
+import model.entities.User;
+
+public class LoginBusiness {
+	
+	private LoginDataAccess loginAccess;
+	private String userName = "";
+	private String password = "";
+	
+	
+	public void setUserName(String userName) {
+		this.userName = userName;
+		
+		if (userName.equals("")) {
+			throw new MessageException("Username not informed.");
+		}
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+		
+		if (password.equals("")) {
+			throw new MessageException("Password not informed.");
+		} 
+
+	}
+	
+	public boolean verifyCredentials() throws ClassNotFoundException, SQLException {
+		User user = new User(userName, password);
+		
+		if(this.getLoginAccess().verifyCredentials(user)) {
+			return true;
+		}
+		else {
+			throw new MessageException("Incorrect credentials.");
+		}		
+	}
+	
+	
+	private LoginDataAccess getLoginAccess() throws ClassNotFoundException, SQLException {
+		if(loginAccess == null) {
+			loginAccess = new LoginDataAccess();
+		}
+		
+		return this.loginAccess;
+		
+	}
+
+}
